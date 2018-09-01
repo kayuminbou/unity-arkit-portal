06f2134ab004967a26d2524b8a035fd83d5e3115
kayuminbou <koyama2199.ssk@gmail.com>
2018-09-01 17:16:33 +0900

    week1

---
+++ Assets/CameraScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+
+    // Update is called once per frame
+    [Header("Cameras")]
+    public Camera mainCam;
+    public Camera portalCam;
+
+    void Update()
+    {
+
+        // Move portal camera position based on main camera distance from the portal.
+        Vector3 cameraOffset = mainCam.transform.position - transform.position;
+        portalCam.transform.position = transform.position + cameraOffset;
+
+        // Make portal cam face the same direction as the main camera.
+        portalCam.transform.rotation = Quaternion.LookRotation(mainCam.transform.forward, Vector3.up);
+    }
+}
